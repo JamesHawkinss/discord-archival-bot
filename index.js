581b309e664161cbab8bec1664b72ba3d37cea40
James Hawkins <jhawki2005@gmail.com>
2020-04-13 08:40:44 +0100

    Log now contains user who reacted, cleaned up code

--- index.js
+++ index.js
@@ -12,24 +12,53 @@ client.on('ready', () => {
 client.on('message', message => {
     if (config.channels.includes(message.channel.id)) {
         message.react('✅');
-        const filter = (reaction, user) => reaction.emoji.name === "✅";
-        let collector = message.createReactionCollector(filter);
-        collector.on('collect', (reaction, collector) => {
-            if (reaction.count === 3) {
-                message.delete().then((message) => {
-                        const channel = client.channels.cache.get(config.log);
-                        const embed = new Discord.MessageEmbed()
-                            .setColor("#2F8ED4")
-                            .setAuthor(message.author.username, message.author.avatarURL())
-                            .setDescription(message.content)
-                            .addField("From", message.channel, true)
-                            .setFooter("✅ complete")
-                            .setTimestamp(Date.now())
-                        channel.send(embed);
-                    });
-            }
-        });
     }
 });
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            return;
+        }
+    }
+    console.log(reaction.count);
+    if (reaction.emoji.name = "✅" && reaction.count === 2) {
+        const reactorUsername = user.username;
+        console.log(user.username);
+        reaction.message.delete()
+            .then((reaction, user => {
+                const channel = client.channels.cache.get(config.log);
+                const embed = new Discord.MessageEmbed()
+                    .setColor("#2F8ED4")
+                    .setAuthor(reaction.message.author.username, reaction.message.author.avatarURL())
+                    .setDescription(reaction.message.content)
+                    .addField("From", reaction.message.channel, true)
+                    .addField("Marked by", reactorUsername, true)
+                    .setFooter("✅ complete")
+                    .setTimestamp(Date.now());
+                channel.send(embed);
+            }));
+    }
+});
+
+// const filter = (reaction, user) => reaction.emoji.name === "✅";
+//         let collector = message.createReactionCollector(filter);
+//         collector.on('collect', (reaction, collector) => {
+//             if (reaction.count === 3) {
+//                 message.delete().then((message) => {
+//                         const channel = client.channels.cache.get(config.log);
+//                         const embed = new Discord.MessageEmbed()
+//                             .setColor("#2F8ED4")
+//                             .setAuthor(message.author.username, message.author.avatarURL())
+//                             .setDescription(message.content)
+//                             .addField("From", message.channel, true)
+//                             .setFooter("✅ complete")
+//                             .setTimestamp(Date.now())
+//                         channel.send(embed);
+//                     });
+//             }
+//         });
 
 
